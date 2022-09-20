2fc4b7a2e1f9f592fc348ca746ae3a65291a6919
ttmunyunguma <ttmunyunguma@gmail.com>
2022-09-20 18:21:54 +0200

    init

---
+++ src/main/java/com/example/filesapi/FilesApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.filesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilesApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FilesApiApplication.class, args);
+    }
+
+}
